9f98524449c4d0f890571c9a4984607a7dfb6992
Shungo Nagamachi <shungo.nagamachi@vacancorp.com>
2026-01-02 01:25:06 +0900

    fix: style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body className="antialiased">
-        <NextTopLoader color="#10b981" showSpinner={false} />
+        <NextTopLoader color="#10b981" height={2} />
         {children}
       </body>
     </html>
